8e6269457c7adb7fdc661947bdf4fbda43903d3e
Juan <51477488+JuanBaezaB@users.noreply.github.com>
2022-10-14 16:46:20 -0300

    darkmode

---
+++ src/components/DarkModeToggle.vue
@@ -0,0 +1,106 @@
+<script setup>
+import { useDark, useToggle } from '@vueuse/core';
+const isDark = useDark();
+const toggleDark = useToggle(isDark);
+</script>
+
+
+<template>
+
+  <!-- Button tipo 1 -->
+  <button type="button" class="p-2
+      text-slate-600 hover:text-slate-800
+      dark:text-slate-300 dark:hover:text-slate-100
+      focus-visible:ring-2 focus-visible:ring-green-400 rounded-lg" @click="toggleDark()">
+    <svg v-if="isDark" class="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
+      <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z" />
+    </svg>
+    <svg v-else class="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
+      <path fill-rule="evenodd"
+        d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
+        clip-rule="evenodd" />
+    </svg>
+  </button>
+
+
+  <!-- Button tipo 2 -->
+  <button type="button" @click="toggleDark()"
+    class="flex justify-center text-violet-600 hover:text-violet-700 self-center p-2 bg-primary-lighten dark:bg-grey-darken-3 text-primary dark:text-primary-300 font-normal uppercase tracking-widest text-sm hover:bg-opacity-30 active:bg-opacity-100 hover:bg-opacity-80 focus-visible:ring-2 focus-visible:ring-primary cursor-pointer rounded-full transition duration-200 no-underline">
+    <div class="flex flex-nowrap items-center whitespace-nowrap justify-center">
+      <svg v-if="isDark" fill="none" viewBox="0 0 24 24" stroke="currentColor" width="24px" height="24px">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+          d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z">
+        </path>
+      </svg>
+      <svg v-else fill="none" viewBox="0 0 24 24" stroke="currentColor" width="24px" height="24px">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+          d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z">
+        </path>
+      </svg>
+    </div>
+  </button>
+
+
+  <!-- Button tipo 3 -->
+  <label for="toggle" class="inline-flex relative items-center cursor-pointer ml-4">
+    <input type="checkbox" id="toggle" class="sr-only peer" :checked="isDark" @click="toggleDark()">
+    <div
+      class="w-11 h-6 bg-slate-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-indigo-300 dark:peer-focus:ring-indigo-800 rounded-full peer dark:bg-slate-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-slate-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-slate-600 peer-checked:bg-indigo-600">
+    </div>
+  </label>
+
+
+
+
+
+  <!-- Button tipo 4 -->
+  <button type="button" @click="toggleDark()"
+    class="flex justify-center text-sky-500 hover:text-sky-700 self-center p-2 bg-primary-lighten dark:bg-grey-darken-3 text-primary dark:text-primary-300 font-normal uppercase tracking-widest text-sm hover:bg-opacity-30 active:bg-opacity-100 hover:bg-opacity-80 focus-visible:ring-2 focus-visible:ring-primary cursor-pointer rounded-full transition duration-200 no-underline">
+    <div class="flex flex-nowrap items-center whitespace-nowrap justify-center">
+      <svg v-if="isDark" viewBox="0 0 24 24" fill="none" class="w-6 h-6">
+        <path fill-rule="evenodd" clip-rule="evenodd"
+          d="M17.715 15.15A6.5 6.5 0 0 1 9 6.035C6.106 6.922 4 9.645 4 12.867c0 3.94 3.153 7.136 7.042 7.136 3.101 0 5.734-2.032 6.673-4.853Z"
+          class="fill-sky-400/20"></path>
+        <path
+          d="m17.715 15.15.95.316a1 1 0 0 0-1.445-1.185l.495.869ZM9 6.035l.846.534a1 1 0 0 0-1.14-1.49L9 6.035Zm8.221 8.246a5.47 5.47 0 0 1-2.72.718v2a7.47 7.47 0 0 0 3.71-.98l-.99-1.738Zm-2.72.718A5.5 5.5 0 0 1 9 9.5H7a7.5 7.5 0 0 0 7.5 7.5v-2ZM9 9.5c0-1.079.31-2.082.845-2.93L8.153 5.5A7.47 7.47 0 0 0 7 9.5h2Zm-4 3.368C5 10.089 6.815 7.75 9.292 6.99L8.706 5.08C5.397 6.094 3 9.201 3 12.867h2Zm6.042 6.136C7.718 19.003 5 16.268 5 12.867H3c0 4.48 3.588 8.136 8.042 8.136v-2Zm5.725-4.17c-.81 2.433-3.074 4.17-5.725 4.17v2c3.552 0 6.553-2.327 7.622-5.537l-1.897-.632Z"
+          class="fill-sky-500"></path>
+        <path fill-rule="evenodd" clip-rule="evenodd"
+          d="M17 3a1 1 0 0 1 1 1 2 2 0 0 0 2 2 1 1 0 1 1 0 2 2 2 0 0 0-2 2 1 1 0 1 1-2 0 2 2 0 0 0-2-2 1 1 0 1 1 0-2 2 2 0 0 0 2-2 1 1 0 0 1 1-1Z"
+          class="fill-sky-500"></path>
+      </svg>
+      <svg v-else fill="none" viewBox="0 0 24 24" stroke="currentColor" width="24px" height="24px">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+          d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z">
+        </path>
+      </svg>
+    </div>
+  </button>
+
+
+
+
+  <button type="button" @click="toggleDark()">
+      
+      <svg v-if="isDark" viewBox="0 0 24 24" fill="none" class="w-6 h-6">
+        <path fill-rule="evenodd" clip-rule="evenodd"
+          d="M17.715 15.15A6.5 6.5 0 0 1 9 6.035C6.106 6.922 4 9.645 4 12.867c0 3.94 3.153 7.136 7.042 7.136 3.101 0 5.734-2.032 6.673-4.853Z"
+          class="fill-sky-400/20"></path>
+        <path
+          d="m17.715 15.15.95.316a1 1 0 0 0-1.445-1.185l.495.869ZM9 6.035l.846.534a1 1 0 0 0-1.14-1.49L9 6.035Zm8.221 8.246a5.47 5.47 0 0 1-2.72.718v2a7.47 7.47 0 0 0 3.71-.98l-.99-1.738Zm-2.72.718A5.5 5.5 0 0 1 9 9.5H7a7.5 7.5 0 0 0 7.5 7.5v-2ZM9 9.5c0-1.079.31-2.082.845-2.93L8.153 5.5A7.47 7.47 0 0 0 7 9.5h2Zm-4 3.368C5 10.089 6.815 7.75 9.292 6.99L8.706 5.08C5.397 6.094 3 9.201 3 12.867h2Zm6.042 6.136C7.718 19.003 5 16.268 5 12.867H3c0 4.48 3.588 8.136 8.042 8.136v-2Zm5.725-4.17c-.81 2.433-3.074 4.17-5.725 4.17v2c3.552 0 6.553-2.327 7.622-5.537l-1.897-.632Z"
+          class="fill-sky-500"></path>
+        <path fill-rule="evenodd" clip-rule="evenodd"
+          d="M17 3a1 1 0 0 1 1 1 2 2 0 0 0 2 2 1 1 0 1 1 0 2 2 2 0 0 0-2 2 1 1 0 1 1-2 0 2 2 0 0 0-2-2 1 1 0 1 1 0-2 2 2 0 0 0 2-2 1 1 0 0 1 1-1Z"
+          class="fill-sky-500"></path>
+      </svg>
+      <svg v-else viewBox="0 0 24 24" fill="none" stroke-width="2" stroke-linecap="round"
+        stroke-linejoin="round" class="w-6 h-6">
+        <path d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" class="fill-sky-400/20 stroke-sky-500"></path>
+        <path
+          d="M12 4v1M17.66 6.344l-.828.828M20.005 12.004h-1M17.66 17.664l-.828-.828M12 20.01V19M6.34 17.664l.835-.836M3.995 12.004h1.01M6 6l.835.836"
+          class="stroke-sky-500"></path>
+      </svg>
+  </button>
+</template>
+
+
+
